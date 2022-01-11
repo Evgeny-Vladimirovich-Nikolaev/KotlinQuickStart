fef262d4361ae139b455c2851f4c76799ee14207
Nobilis <nikolaev-evgeny@yandex.ru>
2022-01-11 14:30:44 +0300

    2022-01-11/14.30 Комментарии

--- src/Hello.kt
+++ src/Hello.kt
@@ -1,6 +1,6 @@
-fun main (args : Array<String>) {
+fun main(args: Array<String>) {
     println("Hello, Kotlin!")
-    var name : String = "Toto"
+    var name: String = "Toto"
     name = name + "shka"
     println(name)
 }
--- src/NullableStrings.kt
+++ src/NullableStrings.kt
@@ -1,18 +1,30 @@
-fun main (args : Array<String>) {
+/**
+ * 1. По умолчанию все объекты в Котлин - не нулабельные: не могут содержать
+ * значение null.
+ * 2. Чтобы сделать объект нулабельным нужно добавить знаки ? после объявления
+ * типа переменной.
+ * 3. Вызвать метод нулабельного типа можно двумя способами:
+ *      а) безопасный способ - varName?.methodName()
+ *      б) небезопасный способ (!!ПЛОХАЯ ПРАКТИКА) - varName!!.methodName()
+ * 4. Чтобы использовать вместо null значение по умолчанию используется
+ * оператор Элвиса - varName?:defaultValue.
+ */
 
-        //Null can not be a value of a non-null type String
-        //var zero : String = null;
+fun main(args: Array<String>) {
 
+    //Null can not be a value of a non-null type String
+    //var zero : String = null;
 
-        //а вот так можно присвоить значение null
-        var zero : String? = null;
+    //а вот так можно присвоить значение null
+    var zero: String? = null;
 
-        var first : String? = null;
-        var second : String? = "four";
-        var third : String? = null;
-        val result : Int = first?.length?:0 + (second?.length?:0) + (third?.length?:0)
-        println(result)
+    var first: String? = null;
+    var second: String? = "four";
+    var third: String? = null;
+    val result: Int = first?.length ?: 0 + (second?.length ?: 0) + (third?.length ?: 0)
+    println(result)
 
-        //барабанная дробь... java.lang(!)NullPointerException
-        println(first!!.length);
-    }
+    //барабанная дробь...
+    println(first!!.length);
+    //java.lang(!)NullPointerException
+}

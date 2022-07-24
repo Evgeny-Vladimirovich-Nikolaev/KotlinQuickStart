c2aec9cb923a92a6e6c49c7b86b30bd35be20675
nobilis <nikolaev-evgeny@yandex.ru>
2022-07-24 20:43:12 +0300

    2022-07-24/20.42 NamesVsZip.kt

--- src/main/KotlinQuickStart/kotlinCode/functions/NamesVsZip.kt
+++ src/main/KotlinQuickStart/kotlinCode/functions/NamesVsZip.kt
@@ -1,4 +1,23 @@
 package kotlinCode.functions
 
-class NamesVsZip {
+val fullNames = mutableListOf<String>()
+
+fun main() {
+    fillfullNames()
+    val separatedList = fullNames.map { Pair(it.substringAfter(' ').substringBefore(' '), it.substringBefore(' '))}
+    println(fullNames)
+    println(separatedList)
+}
+
+fun fillfullNames() {
+    fullNames.add("Иванов Георгий Иванович")
+    fullNames.add("Власов Савва Никитич")
+    fullNames.add("Беляков Артём Фёдорович")
+    fullNames.add("Левина Анастасия Степановна")
+    fullNames.add("Кулешов Иван Артёмович")
+    fullNames.add("Родионов Андрей Никитич")
+    fullNames.add("Романов Артемий Ильич")
+    fullNames.add("Смирнов Андрей Андреевич")
+    fullNames.add("Лебедева Алиса Кирилловна")
+    fullNames.add("Коровина Мария Георгиевна")
 }

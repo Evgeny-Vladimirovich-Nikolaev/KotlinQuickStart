1778d3a42f71aafea7d0f5ef366e14b9a66f03fb
nobilis <nikolaev-evgeny@yandex.ru>
2022-07-25 01:20:35 +0300

    2022-07-25/01.19 ReportVsFlatMap.kt - начало работы.

---
+++ src/main/KotlinQuickStart/kotlinCode/functions/ReportVsFlatMap.kt
@@ -0,0 +1,25 @@
+package kotlinCode.functions
+
+val data = mapOf(
+    "Январь" to listOf(100, 200, 150, 50, 120),
+    "Февраль" to listOf(100, 200, 150, 250),
+    "Март" to listOf(300, 200, 150, 50, 70),
+    "Апрель" to listOf(100, 100, 150, 80),
+    "Май" to listOf(90, 140, 150, 200),
+    "Июнь" to listOf(200, 150, 160, -180),
+    "Июль" to listOf(140, 190, 240, 220, 140),
+    "Август" to listOf(120, 30, 30, 260),
+    "Сентябрь" to listOf(300, 100, 110, 60),
+    "Октябрь" to listOf(100, -200, 150, 50),
+    "Ноябрь" to listOf(100, 120, 150, 400),
+    "Декабрь" to listOf(100, 220, 300, 450, 650)
+)
+
+fun main() {
+    printReport()
+}
+
+fun printReport() {
+
+}
+

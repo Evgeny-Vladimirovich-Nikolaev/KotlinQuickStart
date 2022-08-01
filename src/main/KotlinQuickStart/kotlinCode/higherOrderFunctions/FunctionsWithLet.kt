ed55ce1888b11b62c8660022f30bfa7c7915f77a
Nobilis <nikolaev-evgeny@yandex.ru>
2022-08-01 15:23:58 +0300

    2022-08-01/15.23 FunctionsWithLet.kt выполнено

--- src/main/KotlinQuickStart/kotlinCode/higherOrderFunctions/FunctionsWithLet.kt
+++ src/main/KotlinQuickStart/kotlinCode/higherOrderFunctions/FunctionsWithLet.kt
@@ -1,12 +1,18 @@
 package kotlinCode.higherOrderFunctions
 
-val randomNumbers = mutableListOf<Int?>()
+val randomNumbers: MutableCollection<Int>? = mutableListOf()
 
 fun main() {
-    with(randomNumbers) {
-        for(i in 1..1000) {
-            add((Math.random() * Integer.MAX_VALUE).toInt())
+    randomNumbers?.let {
+        with(randomNumbers) {
+            for (i in 1..1000) {
+                add((Math.random() * Integer.MAX_VALUE).toInt())
+            }
+            val result = filter { it % 2 == 0 }.take(100)
+            for(i in result)
+            {
+                println(i)
+            }
         }
-        println(randomNumbers.first())
     }
 }
